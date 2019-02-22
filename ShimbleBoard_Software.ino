0b39999a84f712d4d2471b0980bbf6704fb5d06b
ajpnq2 <43287531+ajpnq2@users.noreply.github.com>
2019-02-22 16:43:20 -0600

    Working Servo Library and RoveComm

--- ShimbleBoard_Software.ino
+++ ShimbleBoard_Software.ino
@@ -1,5 +1,5 @@
 // Shimble Board ///////////////////////////////////////////////////////////////////////////////////////
-    // Main .cpp File
+// Main .cpp File
 //
 // Created for 2019 Valkyrie by: Andrew Phillips, ajpnq2
 //
@@ -11,29 +11,29 @@ RoveCommEthernetUdp RoveComm; //Extantiates RoveComm class
 
 // Constants //////////////////////////////////////////////////////////////////////////////////////////////
     // Pinout
-#define SERVO_1_CRTL_PIN   PG_1
-#define SERVO_2_CRTL_PIN   PK_4
-#define SERVO_3_CRTL_PIN   PK_5
-#define SERVO_4_CRTL_PIN   PM_0
-#define SERVO_5_CRTL_PIN   PM_7
-#define SERVO_6_CRTL_PIN   PD_2
-#define SERVO_7_CRTL_PIN   PA_4
-#define SERVO_8_CRTL_PIN   PA_5
+#define SERVO_1_CRTL_PIN   PM_7
+#define SERVO_2_CRTL_PIN   PA_7
+#define SERVO_3_CRTL_PIN   PM_6
+#define SERVO_4_CRTL_PIN   PM_1
+#define SERVO_5_CRTL_PIN   PM_0
+#define SERVO_6_CRTL_PIN   PK_5
+#define SERVO_7_CRTL_PIN   PK_4
+#define SERVO_8_CRTL_PIN   PG_1
 
     //Delays
 #define ROVECOMM_DELAY    5
 
     // Declarations
 Servo Servos[8];
-
-void getServoPositions(RC_SHIMBLEBOARD_SERVOPOS_DATAID[]);
+unsigned long telemetry_time = 0;
+uint16_t servo_positions[8];
 
 // Setup & Loop Funcitons /////////////////////////////////////////////////////////////////////////////
 
 void setup()
 {
   Serial.begin(9600);
-  RoveComm.begin(RC_SHIMBLEBOARD_FOURTHOCTET);
+  RoveComm.begin(RC_SHIMBLENAVBOARD_FOURTHOCTET);
   delay(ROVECOMM_DELAY);
 
   Servos[0].attach(SERVO_1_CRTL_PIN);
@@ -56,6 +56,11 @@ void loop()
   // put your main code here, to run repeatedly: 
   rovecomm_packet packet;
   RC_SHIMBLEBOARD_SERVOPOS_DATATYPE servo_posotions[RC_SHIMBLEBOARD_SERVOPOS_DATACOUNT];
+
+  for (int i = 0; i < RC_SHIMBLEBOARD_SERVOPOS_DATACOUNT; i++)
+    {
+      servo_positions[i] = Servos[i].read();
+    }
   
   packet = RoveComm.read();
 
@@ -65,11 +70,9 @@ void loop()
     {
       case RC_SHIMBLEBOARD_SERVOINC_DATAID:
       {
-        RC_SHIMBLEBOARD_SERVOINC_DATATYPE curr_positions[RC_SHIMBLEBOARD_SERVOINC_DATACOUNT];
-        getServoPositions(curr_positions);
         for (int i = 0; i < RC_SHIMBLEBOARD_SERVOINC_DATACOUNT; i++)
         {
-           Servos[i].write(curr_positions[i] + packet.data[i]);
+           Servos[i].write(servo_positions[i] + packet.data[i]);
         }
         break;
       }
@@ -94,17 +97,10 @@ void loop()
     }
   }
 
-  getServoPositions(servo_positions);
-  RoveComm.write(RC_SHIMBLEBOARD_SERVOPOS_DATAID, RC_SHIMBLEBOARD_SERVOPOS_DATACOUNT, servo_positions);
-  delay (ROVECOMM_DELAY);
-}
-
-
-// Functions
-void getServoPositions(RC_SHIMBLEBOARD_SERVOPOS_DATAID servo_positions[RC_SHIMBLEBOARD_SERVOPOS_DATACOUNT])
-{
-  for (int i = 0; i < RC_SHIMBLEBOARD_SERVOPOS_DATACOUNT; i++)
-  {
-    servo_positions[i] = Servos[i].read();
+  if (millis() - telemetry_time >= 1000) // If it's been more than 1 second since telemetry was sent,
+  {                                    // send telemetry and update telemetry_time
+    telemetry_time = millis();
+    RoveComm.write(RC_SHIMBLEBOARD_SERVOPOS_DATAID, RC_SHIMBLEBOARD_SERVOPOS_DATACOUNT, servo_positions);
+    delay (ROVECOMM_DELAY);
   }
 }
