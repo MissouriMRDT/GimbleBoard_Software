20ae32063e3dfd2377cbaffb84fdbf75c7a0726f
Jacob Lipina <jrlwd5@mst.edu>
2019-04-14 01:03:34 -0500

    Rewrote to use servo.writeMicroseconds()

--- ShimbleBoard_Software.cpp
+++ ShimbleBoard_Software.cpp
@@ -10,6 +10,7 @@
 
 // Variable Declarations ///////////////////////////////////////////////////////////////////////////////
 Servo Servos[8];
+//RC_SHIMBLEBOARD_SERVOPOS_DATATYPE servo_positions[RC_SHIMBLEBOARD_SERVOPOS_DATACOUNT];
 uint16_t servo_positions[8];
 uint32_t telemetry_time = 0;
 
@@ -28,41 +29,50 @@ void shimbleSetup() //void setup
   Servos[6].attach(SERVO_7_CRTL_PIN);
   Servos[7].attach(SERVO_8_CRTL_PIN);
 
-  Servos[0].write(SERVO_1_REST);
-  Servos[1].write(SERVO_2_REST);
+  Servos[0].writeMicroseconds(SERVO_1_REST);
+  Servos[1].writeMicroseconds(SERVO_2_REST);
   Servos[2].write(SERVO_3_REST);
   Servos[3].write(SERVO_4_REST);
 
+  servo_positions[0] = SERVO_1_REST;
+  servo_positions[1] = SERVO_2_REST;
+  servo_positions[2] = SERVO_3_REST;
+  servo_positions[3] = SERVO_4_REST;
 
   Serial.println("Shimble Setup Complete.");
 }
 
 void shimbleLoop(rovecomm_packet packet, RoveCommEthernetUdp * RoveComm) //void loop
 { 
-  RC_SHIMBLEBOARD_SERVOPOS_DATATYPE servo_positions[RC_SHIMBLEBOARD_SERVOPOS_DATACOUNT];
 
-  for (int i = 0; i < RC_SHIMBLEBOARD_SERVOPOS_DATACOUNT; i++)
+  for (int i = 1; i < RC_SHIMBLEBOARD_SERVOPOS_DATACOUNT; i++)
   {
-    servo_positions[i] = Servos[i].read();
+    Servos[i].writeMicroseconds(servo_positions[i]);
+    //servo_positions[i] = map(Servos[i].read(), 0, 180, 1000, 2000);
   }
-
+/*
+  Servos[0].writeMicroseconds(SERVO_1_REST);
+  Servos[1].writeMicroseconds(SERVO_2_REST);
+  Servos[2].write(SERVO_3_REST);
+  Servos[3].write(SERVO_4_REST);
+*/
   if(packet.data_id != 0)
   {
     switch (packet.data_id)
     {
       case RC_SHIMBLEBOARD_SERVOINC_DATAID: //Servo Increment
       {
-		int new_pos[8];
+		    int new_pos[8];
 		
-		for (int i = 0; i < RC_SHIMBLEBOARD_SERVOINC_DATACOUNT; i++)
+		    for (int i = 0; i < RC_SHIMBLEBOARD_SERVOINC_DATACOUNT; i++)
         {
-		   new_pos[i] = servo_positions[i] + packet.data[i];
+		      new_pos[i] = servo_positions[i] + packet.data[i];
 	    
-		   // Check new values against servo range
-		   if (new_pos[i] > 180) new_pos[i] = 180;
-		   else if (new_pos[i] < 0) new_pos[i] = 0;
+		      // Check new values against servo range
+		      if (new_pos[i] > 180) new_pos[i] = 180;
+		      else if (new_pos[i] < 0) new_pos[i] = 0;
 		   
-		   Servos[i].write(new_pos[i]);
+		      Servos[i].write(new_pos[i]);
         }
         break;
       }
@@ -70,30 +80,30 @@ void shimbleLoop(rovecomm_packet packet, RoveCommEthernetUdp * RoveComm) //void 
       case RC_SHIMBLEBOARD_MAINGIMBALINC_DATAID: //Main Gimbal Increment
       {
 		
-		if(packet.data[1] < -IGNORE_THRESHOLD)
-		{
-			servo_positions[1] = servo_positions[1] + INC_VALUE;
-			if (servo_positions[1] >= SERVO_2_MAX) {servo_positions[1] = SERVO_2_MAX;}
-		}
-		
-		if(packet.data[1] > IGNORE_THRESHOLD)
-		{
-			servo_positions[1] = servo_positions[1] - INC_VALUE;
-		    if (servo_positions[1] <= SERVO_2_MIN) {servo_positions[1] = SERVO_2_MIN;}
-		}
-		
-    if(packet.data[0] == 0)
-    {
-      servo_positions[0] = SERVO_1_REST;
-    }
-    else
-    {
-		  servo_positions[0] = map(packet.data[0], -100, 100, SERVO_1_MAX, SERVO_1_MIN); // Continuous servo map
-		}
-
-		for (int i = 0; i < RC_SHIMBLEBOARD_MAINGIMBALINC_DATACOUNT; i++)
+		    if(packet.data[1] < -IGNORE_THRESHOLD)
+		    {
+		    	servo_positions[1] = servo_positions[1] + INC_VALUE;
+		    	if (servo_positions[1] >= SERVO_2_MAX) {servo_positions[1] = SERVO_2_MAX;}
+		    }
+		    
+		    if(packet.data[1] > IGNORE_THRESHOLD)
+		    {
+		    	servo_positions[1] = servo_positions[1] - INC_VALUE;
+		      if (servo_positions[1] <= SERVO_2_MIN) {servo_positions[1] = SERVO_2_MIN;}
+		    }
+		    
+        if(packet.data[0] == 0)
+        {
+          servo_positions[0] = SERVO_1_REST;
+        }
+        else
+        {
+		      servo_positions[0] = map(packet.data[0], -100, 100, SERVO_1_MAX, SERVO_1_MIN); // Continuous servo map
+		    }
+    
+		    for (int i = 0; i < RC_SHIMBLEBOARD_MAINGIMBALINC_DATACOUNT; i++)
         {  
-		   Servos[i].write(servo_positions[i]);
+		      Servos[i].writeMicroseconds(servo_positions[i]);
         }
 		
         break;
@@ -101,33 +111,33 @@ void shimbleLoop(rovecomm_packet packet, RoveCommEthernetUdp * RoveComm) //void 
 
       case RC_SHIMBLEBOARD_DRIVEGIMBALINC_DATAID: //Drive Gimbal Increment
       {
-	    if(packet.data[0] > IGNORE_THRESHOLD)
-		{
-			servo_positions[2] = servo_positions[2] - INC_VALUE;
-		    if (servo_positions[2] <= SERVO_3_MIN) servo_positions[2] = SERVO_3_MIN;
-		}
-
-		if(packet.data[0] < -IGNORE_THRESHOLD)
-		{
-			servo_positions[2] = servo_positions[2] + INC_VALUE;
-			if (servo_positions[2] >= SERVO_3_MAX) servo_positions[2] = SERVO_3_MAX;
-		}
-		
-		if(packet.data[1] > IGNORE_THRESHOLD)
-		{
-			servo_positions[3] = servo_positions[3] - INC_VALUE;
-			if (servo_positions[3] <= SERVO_4_MIN) servo_positions[3] = SERVO_4_MIN;
-		}
-		
-		if(packet.data[1] < -IGNORE_THRESHOLD)
-		{
-			servo_positions[3] = servo_positions[3] + INC_VALUE;
-			if (servo_positions[3] >= SERVO_4_MAX) servo_positions[3] = SERVO_4_MAX;
-		}
-			
-		for (int i = 0; i < RC_SHIMBLEBOARD_DRIVEGIMBALINC_DATACOUNT; i++)
+	      if(packet.data[0] > IGNORE_THRESHOLD)
+		    {
+		    	servo_positions[2] = servo_positions[2] - INC_VALUE;
+		      if (servo_positions[2] <= SERVO_3_MAX) servo_positions[2] = SERVO_3_MAX;
+		    }
+    
+		    if(packet.data[0] < -IGNORE_THRESHOLD)
+		    {
+		    	servo_positions[2] = servo_positions[2] + INC_VALUE;
+		    	if (servo_positions[2] >= SERVO_3_MIN) servo_positions[2] = SERVO_3_MIN;
+		    }
+		    
+		    if(packet.data[1] > IGNORE_THRESHOLD)
+		    {
+		    	servo_positions[3] = servo_positions[3] - INC_VALUE;
+		    	if (servo_positions[3] <= SERVO_4_MIN) servo_positions[3] = SERVO_4_MIN;
+		    }
+		    
+		    if(packet.data[1] < -IGNORE_THRESHOLD)
+		    {
+		    	servo_positions[3] = servo_positions[3] + INC_VALUE;
+		    	if (servo_positions[3] >= SERVO_4_MAX) servo_positions[3] = SERVO_4_MAX;
+		    }
+		    	
+		    for (int i = 0; i < RC_SHIMBLEBOARD_DRIVEGIMBALINC_DATACOUNT; i++)
         {  
-		   Servos[i + 2].write(servo_positions[i + 2]);
+		      Servos[i + 2].write(servo_positions[i + 2]);
         }
         break;
       }
@@ -136,8 +146,8 @@ void shimbleLoop(rovecomm_packet packet, RoveCommEthernetUdp * RoveComm) //void 
       {
         for (int i = 0; i < RC_SHIMBLEBOARD_SERVOABSOLUTE_DATACOUNT; i++)
         {
-           map(packet.data[i], -100, 100, 0, 180);
-		   Servos[i].write(packet.data[i]);
+          map(packet.data[i], -100, 100, 0, 180);
+		      Servos[i].write(packet.data[i]);
         }
         break;    
       }
@@ -145,27 +155,27 @@ void shimbleLoop(rovecomm_packet packet, RoveCommEthernetUdp * RoveComm) //void 
       case RC_SHIMBLEBOARD_MAINGIMBALABS_DATAID: //Main Gimbal Abolute
       {
         int mapped_pos[2];
-		Serial.print("data[0]: ");
-		Serial.println(packet.data[0]);
-		
-		mapped_pos[0] = map(packet.data[0], -100, 100, 85, 94); // Continuous servo map
-		mapped_pos[1] = map(packet.data[1], -100, 100, 120, 180); // Rest state: 150
-		
-		Serial.print("mapped pos[0]: ");
-		Serial.println(mapped_pos[0]);
-		
-		if (mapped_pos[0] < 92 && mapped_pos[0] > 87)
-		{
-			mapped_pos[0] = 89;
-		}
-		
-		Servos[0].write(mapped_pos[0]);
-		Servos[1].write(mapped_pos[1]);
-		
-		for (int i = 0; i < RC_SHIMBLEBOARD_MAINGIMBALABS_DATACOUNT; i++)
-        {  
-		//   Servos[i].write(mapped_pos[i]);
-        }
+		    Serial.print("data[0]: ");
+		    Serial.println(packet.data[0]);
+		    
+		    mapped_pos[0] = map(packet.data[0], -100, 100, 85, 94); // Continuous servo map
+		    mapped_pos[1] = map(packet.data[1], -100, 100, 120, 180); // Rest state: 150
+		    
+		    Serial.print("mapped pos[0]: ");
+		    Serial.println(mapped_pos[0]);
+		    
+		    if (mapped_pos[0] < 92 && mapped_pos[0] > 87)
+		    {
+		    	mapped_pos[0] = 89;
+		    }
+		    
+		    Servos[0].write(mapped_pos[0]);
+		    Servos[1].write(mapped_pos[1]);
+		    
+		    for (int i = 0; i < RC_SHIMBLEBOARD_MAINGIMBALABS_DATACOUNT; i++)
+            {  
+		    //   Servos[i].write(mapped_pos[i]);
+            }
         break;    
       }
 
@@ -173,12 +183,12 @@ void shimbleLoop(rovecomm_packet packet, RoveCommEthernetUdp * RoveComm) //void 
       {
         int mapped_pos[2];
 		
-		mapped_pos[0] = map(packet.data[0], -100, 100, 0, 180); // Rest state: 75
-		mapped_pos[1] = map(packet.data[1], -100, 100, 0, 180); // Rest state: 15
-		
-		for (int i = 0; i < RC_SHIMBLEBOARD_DRIVEGIMBALABS_DATACOUNT; i++)
+		    mapped_pos[0] = map(packet.data[0], -100, 100, 0, 180); // Rest state: 75
+		    mapped_pos[1] = map(packet.data[1], -100, 100, 0, 180); // Rest state: 15
+		    
+		    for (int i = 0; i < RC_SHIMBLEBOARD_DRIVEGIMBALABS_DATACOUNT; i++)
         {
-		   Servos[i + 2].write(mapped_pos[i]);
+		      Servos[i + 2].write(mapped_pos[i]);
         }
         break;    
       }
--- ShimbleBoard_Software.h
+++ ShimbleBoard_Software.h
@@ -21,27 +21,27 @@
 #define SERVO_7_CRTL_PIN   PK_4
 #define SERVO_8_CRTL_PIN   PG_1
     //Delays
-#define ROVECOMM_DELAY        5
+#define ROVECOMM_DELAY     5
 
 	//Misc Values
-#define IGNORE_THRESHOLD      5
-#define INC_VALUE             3
+#define IGNORE_THRESHOLD   50
+#define INC_VALUE          35
 
 	//Servo Positions
-#define SERVO_1_REST         90
-#define SERVO_2_REST        85
-#define SERVO_3_REST        126
-#define SERVO_4_REST         90
+#define SERVO_1_REST       1472 //1470 is boundary in which it stops drifting to right; 1476 is boundary in which it stops drifting left; 1473 is middle, no drifting
+#define SERVO_2_REST       1400 //Main cam pitch facing up. Can lower value to face further upwards.
+#define SERVO_3_REST       1830 //Drive cam pan facing forward.
+#define SERVO_4_REST       1500 //Drive cam pitch facing forward.
 
-#define SERVO_1_MAX          94
-#define SERVO_2_MAX         180
-#define SERVO_3_MAX         162
-#define SERVO_4_MAX         180
+#define SERVO_1_MAX        1523 //Change value to change how fast the pan moves left and right. 
+#define SERVO_2_MAX        2000 //Main cam pitch facing down; Higher than 2000 faces camera directly into 3d print.
+#define SERVO_3_MAX        1400 //Drive cam pan max right; Lower than 1400 causes servo to jitter.
+#define SERVO_4_MAX        2200 //Drive cam pitch facing down; Higher than 2200 causes servo to jitter.
 
-#define SERVO_1_MIN          83
-#define SERVO_2_MIN          64
-#define SERVO_3_MIN          90
-#define SERVO_4_MIN           0
+#define SERVO_1_MIN        1423 //Change value to change how fast the pan moves left and right.
+#define SERVO_2_MIN        1000
+#define SERVO_3_MIN        2230 //Drive cam pan max left; Higher than 2230 causes servo to jitter.
+#define SERVO_4_MIN        750 //Drive cam pitch facing up; Lower than 750 causes servo to jitter.
 
 // Function Declarations ////////////////////////////////////////////////////////////////////////////////
 void shimbleSetup();
